36e44978f64c4fddfb5c15aa29b6182bedc5e841
BB~ <89837485+beats64@users.noreply.github.com>
2021-08-31 17:02:19 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,210 @@
-# SKY-BOX-main
+# SKY-BOX
+BlueSkyXN  综合工具箱 Linux Supported ONLY 仅供个人非商用自用 版权/功能等问题请发issue
+
+BlueSkyXN Comprehensive Toolbox Linux only supports
+
+https://www.blueskyxn.com/202104/4465.html
+https://www.blueskyxn.com/202105/4585.html
+
+## 使用方法
+
+wget -O box.sh https://raw.githubusercontent.com/BlueSkyXN/SKY-BOX/main/box.sh && chmod +x box.sh && clear && ./box.sh
+
+<pre>wget -O box.sh https://raw.githubusercontent.com/BlueSkyXN/SKY-BOX/main/box.sh && chmod +x box.sh && clear && ./box.sh</pre>
+
+### ARM beta使用方法
+<details><summary>ARM beta使用方法</summary>
+wget -O box.sh https://raw.githubusercontent.com/BlueSkyXN/SKY-BOX/main/armbox.sh && chmod +x box.sh && clear && ./box.sh
+
+<pre>wget -O box.sh https://raw.githubusercontent.com/BlueSkyXN/SKY-BOX/main/armbox.sh && chmod +x box.sh && clear && ./box.sh</pre>
+</details>
+
+# 功能表
+
+<details><summary>功能表</summary>
+
+IPV.SH ipv4/6优先级调整一键脚本·下载
+
+IPT.SH iptable一键脚本
+
+SpeedTest-Linux 下载
+
+Rclone&Fclone·下载
+
+ChangeSource Linux换源脚本·下载
+
+Besttrace 路由追踪·下载
+
+NEZHA.SH哪吒面板/探针·下载
+
+获取本机IP
+
+安装最新BBR内核·使用YUM·仅支持CentOS
+
+启动BBR FQ算法
+
+系统网络配置优化
+
+Git 新版 安装·仅支持CentOS
+
+宝塔面板 自动磁盘挂载工具
+
+BBR一键管理脚本
+
+SWAP一键安装/卸载脚本
+
+Superbench 综合测试
+
+MT.SH 流媒体解锁测试
+
+Lemonbench 综合测试
+
+UNIXbench 综合测试
+
+三网Speedtest测速
+
+Memorytest 内存压力测试
+
+Route-trace 路由追踪测试
+
+YABS LINUX综合测试
+
+Disk Test 硬盘&系统综合测试
+
+TubeCheck Google/Youtube CDN分配节点测试
+
+MTP&TLS 一键脚本
+
+Rclone官方一键安装脚本
+
+Aria2 最强安装与管理脚本
+
+甲骨文ARM U20 DD Debian 10
+
+宝塔面板综合安装脚本
+
+</details>
+  
+## 宝塔面板综合安装脚本
+<details><summary>宝塔面板综合安装脚本</summary>
+  
+宝塔面板 官方版 v7.5.2
+
+CentOS
+
+Ubuntu&Deepin
+
+Python3通用版
+
+升级与更新
+
+宝塔面板 Hostcli 净化版 v7.4.5
+
+一键安装·Centos
+
+一键转移/升级
+
+宝塔面板 Fenhao 开心版 v7.5.2
+
+CentOS
+
+Ubuntu&Deepin
+
+Python3通用版
+
+升级与更新
+
+</details>
+
+# 声明
+
+大部分功能非我原创，部分功能仅提供辅助调用其他作者的产品
+
+不提供/保证任何功能的可用性，安全性，有效性，合法性
+
+请确定你的用途符合 PRC(Mainland) USA 相关法律法规的要求和限制
+
+请确定你的用途符合 你当地和运行环境/设备的 相关法律法规的要求和限制
+
+## 外联作品列表
+<details><summary>外联作品列表</summary>
+外联作品包括但不限于
+
+
+Rclone魔改 https://hostloc.com/thread-612579-1-1.html
+
+Rclone https://rclone.org
+
+Fclone https://github.com/mawaya/rclone
+
+宝塔 https://www.bt.cn
+
+HostCli https://www.hostcli.com
+
+Fenhao https://fenhao.me
+
+Aria2 https://github.com/P3TERX/aria2.sh
+
+Fio-bench https://github.com/amefs/fio-bench
+
+DiskTest https://github.com/Aniverse/A
+
+TubeCheck https://github.com/sjlleo/TubeCheck
+
+Nezha https://github.com/naiba/nezha
+
+Unixbench https://teddysun.com/245.html
+
+IPT https://github.com/arloor/iptablesUtils
+
+MT https://github.com/CoiaPrant/MediaUnlock_Test
+
+Besttrace https://www.ipip.net
+
+BBR管理脚本/TCPSH https://blog.ylx.me/archives/783.html
+
+YABS https://github.com/masonr/yet-another-bench-script
+
+SuperBench https://www.oldking.net/350.html
+
+Route-trace https://github.com/BlueSkyXN/Route-trace
+
+YUM-GIT https://github.com/BlueSkyXN/Yum-Git
+
+BBRSH https://github.com/BlueSkyXN/ChangeSource/blob/master/bbr.sh
+
+IPV https://github.com/BlueSkyXN/ChangeSource/blob/master/ipv.sh
+
+Debian Network Reinstall https://github.com/bohanyang/debi
+
+MTPTLS https://github.com/sunpma/mtp
+
+Memorytest https://github.com/FunctionClub/Memtester
+
+Superspeed https://github.com/ernisn/superspeed
+
+</details>
+
+其他没有说明的，均为找不着来源了，欢迎issue反馈
+<details><summary>失联作品列表</summary>
+失联作品包括但不限于
+swap.sh 
+
+changesource.sh
+</details>
+
+## 编写规范和友情提醒
+
+不遵循任何规范，我就喜欢随意，反正自用能动就行
+
+基本操作：wget到root然后权限拉满
+
+如果你对规范要求较高，请自行修改/编写
+
+我不是大佬，也不靠这个吃饭，你需要厉害的作品请找大佬们
+
+本作仅供个人非商用自用，0现金收入
+
+有问题看心情解决，版权/功能等问题请发issue
+
+爱用不用，反正只是低端作品罢了，觉得还可以的话希望右上角点个星标
---
+++ btbox.sh
@@ -0,0 +1,166 @@
+#! /bin/bash
+# By BlueSkyXN
+#https://github.com/BlueSkyXN/SKY-BOX
+
+#彩色
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+
+
+
+#宝塔面板 官方版
+#一键安装
+#CentOS
+function btof1(){
+yum install -y wget && wget -O install.sh http://download.bt.cn/install/install_6.0.sh && sh install.sh
+}
+#Ubuntu&Deepin
+function btof2(){
+wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
+}
+#Python3通用版
+function btof3(){
+curl -sSO http://download.bt.cn/install/install_panel.sh && bash install_panel.sh
+}
+#升级与更新
+function btof4(){
+curl http://download.bt.cn/install/update6.sh|bash
+}
+
+#宝塔面板 Hostcli 净化版 v7.6.0
+#Centos
+function btcli1(){
+yum install -y wget && wget -O install.sh http://v7.hostcli.com/install/install_6.0.sh --no-check-certificate && sh install.sh
+}
+#Ubuntu&Deepin
+function btcli2(){
+wget -O install.sh http://v7.hostcli.com/install/install-ubuntu_6.0.sh --no-check-certificate && sudo bash install.sh
+}
+#一键转移/升级
+function btcli3(){
+curl http://v7.hostcli.com/install/update6.sh|bash
+}
+
+#宝塔面板 Fenhao 开心版 v7.6.0
+#CentOS
+function btfh1(){
+yum install -y wget && wget -O install.sh https://download.seele.wang/ltd/install/install_6.0.sh --no-check-certificate && sh install.sh
+}
+
+#CentOS-old
+function btfh1b(){
+yum install -y wget && wget -O install.sh https://download.fenhao.me/ltd/install/install_6.0.sh --no-check-certificate && sh install.sh
+}
+
+#Ubuntu&Deepin
+function btfh2(){
+wget -O install.sh https://download.seele.wang/ltd/install/install-ubuntu_6.0.sh --no-check-certificate && sudo bash install.sh
+}
+#Python3通用版
+function btfh3(){
+curl -sSO https://download.seele.wang/ltd/install/install_panel.sh -k && bash install_panel.sh
+}
+#升级与更新
+function btfh4(){
+curl -sSO https://download.seele.wang/ltd/install/install_panel.sh -k && bash install_panel.sh
+}
+#ARM 定制版
+function btfh5(){
+curl -sSO https://download.seele.wang/ltd/install/arm/install_panel.sh -k && bash install_panel.sh
+}
+#升级与更新ARM定制版
+function btfh6(){
+wget -O updatearm.sh https://download.seele.wang/ltd/install/arm/updatearm.sh --no-check-certificate && bash updatearm.sh
+}
+
+#主菜单
+function start_menu(){
+    clear
+    red " BlueSkyXN 综合工具箱 Linux Supported ONLY" 
+    red " 宝塔面板综合安装脚本合集" 
+    green " FROM: https://github.com/BlueSkyXN/SKY-BOX "
+    green " HELP: https://www.blueskyxn.com/202104/4465.html "
+    green " USE:  wget -O btbox.sh https://raw.githubusercontent.com/BlueSkyXN/SKY-BOX/main/btbox.sh && chmod +x btbox.sh && clear && ./btbox.sh "
+    yellow " =================================================="
+    red "宝塔面板 官方版 v7.6.0"
+    green " 11. CentOS"
+    green " 12. Ubuntu&Deepin"
+    green " 13. Python3通用版"
+    green " 14. 升级与更新"
+    red "宝塔面板 Hostcli 净化版 v7.6.0"
+    green " 21. Centos"
+    green " 22. Ubuntu&Deepin"
+    green " 23. 一键转移/升级"
+    red "宝塔面板 Fenhao 开心版 v7.6.0 还在咕咕咕可能不能用"
+    green " 31. CentOS"
+    green " 32. Ubuntu&Deepin"
+    green " 33. Python3通用版"
+    green " 34. 升级与更新"
+    green " 35. ARM 定制版"
+    green " 36. 升级与更新ARM定制版【请先卸载NGINX等运行环境和全部插件】"
+    green " =================================================="
+    green " 0. 退出脚本"
+    echo
+    read -p "请输入数字:" menuNumberInput
+    case "$menuNumberInput" in
+
+	11 )
+           btof1
+	;;
+	12 )
+           btof2
+	;;
+	13 )
+           btof3
+	;;
+	14 )
+           btof4
+	;;
+	21 )
+           btcli1
+	;;
+	22 )
+           btcli2
+	;;
+	23 )
+           btcli3
+	;;
+	31 )
+           btfh1
+	;;
+	32 )
+           btfh2
+	;;
+	33 )
+           btfh3
+	;;
+	34 )
+           btfh4
+	;;
+	35 )
+           btfh5
+	;;
+	36 )
+           btfh6
+	;;
+        0 )
+            exit 1
+        ;;
+        * )
+            clear
+            red "请输入正确数字 !"
+            start_menu
+        ;;
+    esac
+}
+start_menu "first"
